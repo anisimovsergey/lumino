ef5a8aaa60f0da73e35ebf40172082ae85b2fbb7
Sergey Anisimov <anisimov.sergey@gmail.com>
2016-12-25 15:15:46 +0300

    Initial commit

---
+++ Lumino/DeviceDetailsUIViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DeviceDetailsUIView.swift
+//  Lumino
+//
+//  Created by Sergey Anisimov on 23/12/2016.
+//  Copyright © 2016 Sergey Anisimov. All rights reserved.
+//
+
+import UIKit
+
+class DeviceDetailsUIViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    var text = String()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.text = text
+    }
+
+    
+
+}
---
+++ Lumino/DevicesTableViewController.swift
@@ -0,0 +1,92 @@
+//
+//  DevicesTableViewController.swift
+//  Lumino
+//
+//  Created by Sergey Anisimov on 19/12/2016.
+//  Copyright © 2016 Sergey Anisimov. All rights reserved.
+//
+
+import UIKit
+
+class DevicesTableViewController: UITableViewController, NetServiceBrowserDelegate, NetServiceDelegate {
+    var nsb : NetServiceBrowser!
+    var services = [NetService]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.start()
+    }
+    
+    func start()  {
+        print("listening for services...")
+        self.services.removeAll()
+        let aNetService = NetService()
+        self.services.append(aNetService)
+        self.nsb = NetServiceBrowser()
+        self.nsb.delegate = self
+        self.nsb.searchForServices(ofType:"_http._tcp", inDomain: "")
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.services.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DeviceCell", for: indexPath)
+        
+        cell.textLabel?.text = services[indexPath.row].name
+        
+        return cell
+    }
+    
+    
+    func updateInterface() {
+        for service in self.services {
+            if service.port == -1 {
+                print("service \(service.name) of type \(service.type)" +
+                    " not yet resolved")
+                service.delegate = self
+                service.resolve(withTimeout:10)
+            } else {
+                print("service \(service.name) of type \(service.type)," +
+                    "port \(service.port), addresses \(service.addresses)")
+            }
+        }
+        self.tableView.reloadData()
+    }
+    
+    func netServiceDidResolveAddress(_ sender: NetService) {
+        self.updateInterface()
+    }
+    
+    func netServiceBrowser(_ aNetServiceBrowser: NetServiceBrowser, didFind aNetService: NetService, moreComing: Bool) {
+        print("adding a service " + aNetService.name)
+        self.services.append(aNetService)
+        if !moreComing {
+            self.updateInterface()
+        }
+    }
+    
+    func netServiceBrowser(_ aNetServiceBrowser: NetServiceBrowser, didRemove aNetService: NetService, moreComing: Bool) {
+        if let ix = self.services.index(of:aNetService) {
+            self.services.remove(at:ix)
+            print("removing a service")
+            if !moreComing {
+               self.updateInterface()
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetails"{
+            if let nextViewController = segue.destination as? DeviceDetailsUIViewController{
+                nextViewController.text = "test"
+            }
+        }
+    }
+}
