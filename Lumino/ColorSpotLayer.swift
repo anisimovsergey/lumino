ce6a1f225d1600e068a9798353cd79fad9a86555
Sergey Anisimov <anisimov.sergey@gmail.com>
2017-05-22 23:45:19 +0100

    Dropping a shadow

--- Lumino/ColorSpotLayer.swift
+++ Lumino/ColorSpotLayer.swift
@@ -15,5 +15,12 @@ class ColorSpotLayer: CAShapeLayer {
         let path = UIBezierPath(ovalIn: self.bounds.insetBy(dx: lineWidth, dy: lineWidth))
         self.path = path.cgPath
         self.strokeColor = UIColor.white.cgColor
+        
+        self.masksToBounds = false
+        self.shadowColor = UIColor.black.cgColor
+        self.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        self.shadowRadius = 4
+        self.shadowOpacity = 0.8
+        self.shadowPath = path.cgPath
     }
 }
